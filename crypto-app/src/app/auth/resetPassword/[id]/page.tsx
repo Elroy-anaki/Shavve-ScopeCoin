d4a3ceaa84bf1d169c51aff2678efb0f92cecc35
@ElroyAnaki <elroy.a1912@gmail.com>
2025-05-07 02:18:56 +0300

    a little fix for prod

--- crypto-app/src/app/auth/resetPassword/[id]/page.tsx
+++ crypto-app/src/app/auth/resetPassword/[id]/page.tsx
@@ -2,19 +2,13 @@ import { ResetPasswordSection } from "@/components/auth/ResetPasswordSection";
 import { authOptions, isSession } from "@/utils/auth/auth";
 import { redirect } from "next/navigation";
 
-interface IResetPasswordProps {
-    params: { id: string };
 
-}
-export default async function Page({ params }: IResetPasswordProps) {
-     const session = await isSession(authOptions)
+export default async function Page({params,}: {params: { id: string }}) {
+  const session = await isSession(authOptions)
         if(session) {
             redirect("/")
         }
-    
-    
       const { id } = params;
-
       return (
         <>
         <div className="mt-20 border-2 border-purple-100 w-1/3 mx-auto py-5 rounded-2xl">
--- crypto-app/src/app/auth/verifyAccount/[id]/page.tsx
+++ crypto-app/src/app/auth/verifyAccount/[id]/page.tsx
@@ -2,26 +2,21 @@ import { VerifyAccountSection } from "@/components/auth/VerifyAccountSection";
 import { authOptions, isSession } from "@/utils/auth/auth";
 import { redirect } from "next/navigation";
 
-type IVerifyAccountProps = {
-  params: { id: string };
-};
 
-export default async function Page({ params }: IVerifyAccountProps){
+export default async function Page({params,}: {params: { id: string }}) {
 
-    const session = await isSession(authOptions)
-    if(session) {
-        redirect("/")
-    }
-
+  const session = await isSession(authOptions)
+  if (session) {
+    redirect("/")
+  }
 
   const { id } = params;
-
   return (
     <>
-    <div className="mt-20 border-2 border-purple-100 w-1/3 mx-auto py-5 rounded-2xl">
+      <div className="mt-20 border-2 border-purple-100 w-1/3 mx-auto py-5 rounded-2xl">
 
-    <VerifyAccountSection id={id}/>
-    </div>
+        <VerifyAccountSection id={id} />
+      </div>
     </>
   );
 };
