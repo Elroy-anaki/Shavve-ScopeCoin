8c1885e080a45178dcd3dd5985c2c32a0d8c027c
@ElroyAnaki <elroy.a1912@gmail.com>
2025-05-07 02:32:12 +0300

    another fix for prod

--- crypto-app/src/app/auth/resetPassword/[id]/page.tsx
+++ crypto-app/src/app/auth/resetPassword/[id]/page.tsx
@@ -3,12 +3,12 @@ import { authOptions, isSession } from "@/utils/auth/auth";
 import { redirect } from "next/navigation";
 
 
-export default async function Page({params,}: {params: { id: string }}) {
+export default async function Page({params}: {params: Promise<{ id: string }>}) {
   const session = await isSession(authOptions)
         if(session) {
             redirect("/")
         }
-      const { id } = params;
+      const { id } = await params;
       return (
         <>
         <div className="mt-20 border-2 border-purple-100 w-1/3 mx-auto py-5 rounded-2xl">
