d46f4158425a89c0250ec834e638c0fad815825e
@ElroyAnaki <elroy.a1912@gmail.com>
2025-05-07 05:45:13 +0300

    Error Handling Fixes #19

--- crypto-app/src/server/routers/auth.ts
+++ crypto-app/src/server/routers/auth.ts
@@ -1,43 +1,42 @@
 import { signUpSchema } from '@/validation/auth/signUpSchema';
 import { publicProcedure, router } from '../trpcConfig';
-import {  resetPassword, signUp, verifyAccount } from '@/requests/db/auth/requests';
+import { resetPassword, signUp, verifyAccount } from '@/requests/db/auth/requests';
 import { z } from 'zod';
 import { sendResetPasswordMail } from '@/requests/db/auth/requests';
 import { resetPasswordSchema } from '@/validation/auth/resetPasswordSchema';
-
+import { handleError } from '../errorHandling'; 
 
 export const authRouter = router({
   signUp: publicProcedure
     .input(signUpSchema)
     .mutation(async ({ input }) => {
-      try {
-        const newUser = await signUp(input);
-      return newUser;
-      } catch (error) {
-        throw error
-      }
-      
+      return handleError(async () => {
+        const newUser = await signUp(input); 
+        return newUser;
+      });
     }),
-    verifyAccount: publicProcedure.input(z.object({id: z.string()})).mutation(async({input}) => {
-      try {
-       await verifyAccount(input.id)
-       
-      } catch (error) {
-        throw error
-      }
+
+  verifyAccount: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input }) => {
+      return handleError(async () => {
+        await verifyAccount(input.id);  
+      });
     }),
-    sendResetPasswordEmail: publicProcedure.input(z.object({userEmail: z.string()})).mutation(async({input}) => {
-      try {
-        await sendResetPasswordMail(input.userEmail)
-      } catch (error) {
-        throw error
-      }
+
+  sendResetPasswordEmail: publicProcedure
+    .input(z.object({ userEmail: z.string() }))
+    .mutation(async ({ input }) => {
+      return handleError(async () => {
+        await sendResetPasswordMail(input.userEmail);  
+      });
     }),
-    resetPassword: publicProcedure.input(resetPasswordSchema).mutation(async({input}) => {
-      try {
-        await resetPassword(input.resetPasswordId, input.userPassword)
-      } catch (error) {
-        throw error
-      }
-    })
+
+  resetPassword: publicProcedure
+    .input(resetPasswordSchema)
+    .mutation(async ({ input }) => {
+      return handleError(async () => {
+        await resetPassword(input.resetPasswordId, input.userPassword);  
+      });
+    }),
 });
